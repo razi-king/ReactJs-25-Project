10c6fe04f364e8628a1d86aef5440f3a055e4793
razi-king <raziinsane@gmail.com>
2025-03-27 05:16:29 -0700

    Third Project Done

--- third-project/src/StarRating/StarRating.jsx
+++ third-project/src/StarRating/StarRating.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useState } from 'react';
 import {FaStar} from 'react-icons/fa'
-export default function StarRating() {
+export default function StarRating({noOfStars=5}) {
+    const [rating,setRating] = useState(0);
+    const [hover,setHover] = useState(0);
+    function handleClick(getIndex){
+        setRating(getIndex);
+    }
+    function handleMouseEnter(getIndex){
+        setHover(getIndex);
+        
+    }
+    function handleMouseLeave(){
+        setHover(rating);
+    }
   return (
-    <div></div>
+    
+    <>
+        <div className="flex gap-2"> {/* Stars ek row mein dikhane ke liye flex use kiya */}
+            {Array.from({ length: noOfStars }).map((_, index) => {
+                const starIndex = index + 1;
+                return (
+                    <FaStar
+                        key={starIndex}
+                        className={starIndex <= (hover || rating) ? 'text-[#fff700]' : 'text-black'}
+                        onClick={() => handleClick(starIndex)}
+                        onMouseEnter={() => handleMouseEnter(starIndex)}
+                        onMouseLeave={handleMouseLeave}
+                        size={40}
+                        
+                    />
+                );
+            })}
+        </div>
+
+        
+    </>    
   )
 }
