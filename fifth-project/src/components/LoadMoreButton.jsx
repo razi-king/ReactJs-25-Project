5626598431aa780cdb2d6049a6ffb45348fabfce
razi-king <raziinsane@gmail.com>
2025-04-06 05:48:18 -0700

    Fifth Project Done

--- fifth-project/src/components/LoadMoreButton.jsx
+++ fifth-project/src/components/LoadMoreButton.jsx
@@ -1,27 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 export default function LoadMoreButton() {
-    const [loading,setLoading] = useState(false);
-    const [products,setProducts]= useState([]);
-    const [count,setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
 
-    async function fetchProduct(){
-        try{
-            const response = await fetch(`https://dummy.json.com/products?limit=20&${count===0 ? 0:count*20}`);
-            const result = await response.json();
-        }catch(e){
-            console.log(e);
-        }
+  async function fetchProduct() {
+    try {
+      setLoading(true);
+      const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count * 20}`);
+      const result = await response.json();
+      if (result && result.products && result.products.length) {
+        setProducts((result.products));
+      }
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      console.log(e);
     }
-    useEffect(()=>{
-        fetchProduct()
-    },[])
+  }
+
+  useEffect(() => {
+    fetchProduct();
+  }, [count]);
+
+  if (loading) {
+    return <div>Loading Data | Please Wait ...</div>;
+  }
+
   return (
     <div>
-        <div>
-            <h1 className='text-3xl font-bold '>Load More Button</h1>
-
-        </div>
+      <div className='grid grid-cols-1 p-2 md:grid-cols-3 xl:grid-cols-4 gap-[10px]'>
+        {products && products.length
+          ? products.map((item) => (
+              <div className='border p-2' key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p> {/* Corrected: Removed quotes */}
+              </div>
+            ))
+          : null}
+      </div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Load More Button</button>
+      </div>
     </div>
-  )
+  );
 }
