89cb9f5797e3537ca5d5e01355cecb47fd95e2b9
razi-king <raziinsane@gmail.com>
2025-04-07 01:34:18 -0700

    Eight Project Done

--- eight-project/src/components/LightDarkMode.jsx
+++ eight-project/src/components/LightDarkMode.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import UseLocalStorage from './UseLocalStorage'
 
 export default function LightDarkMode() {
+    const [theme,setTheme]=UseLocalStorage('theme','black');
+    function handleToggleTheme(){
+        setTheme(theme==='black' ? "white":'black'
+        );
+        console.log(theme);
+        
+    }
   return (
-    <div></div>
+    <div className={`min-h-screen text-center m-auto pt-[50%] ${theme==='black' ? 'bg-black text-white' :'bg-white text-black' }`}>
+        <div>
+            <h1 className='font-bold text-2xl'>Hello World</h1>
+            <button onClick={handleToggleTheme} className='font-bold text-xl border-2 hover:bg-blue-400'>Change Theme</button>
+        </div>
+    </div>
   )
 }
